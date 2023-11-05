d5420ff7767627cd2997d7ebc1ad689b3d003a65
Ikuya Yamada <ikuya@ikuya.net>
2023-11-05 22:44:34 +0900

    set no-templates as default

--- scripts/extract_wikipedia_articles.sh
+++ scripts/extract_wikipedia_articles.sh
@@ -2,14 +2,11 @@
 
 for LANG in ${TARGET_LANGUAGES}
 do
-    if [ ${LANG} = "zh" ]; then
-        ARGS="--no-templates"
-        echo "Using --no-templates"
-    fi
     wikiextractor \
         ${WIKIPEDIA_DATA_DIR}/${LANG}wiki-${WIKIPEDIA_DUMP_DATE}-pages-articles-multistream.xml.bz2 \
         -o ${WIKIPEDIA_DATA_DIR}/${LANG} \
         --html-safe "" \
         --links \
+        --no-templates \
         ${ARGS}
 done
