d034df8178a87617919fce7e04ed741fbbc28f65
Ikuya Yamada <ikuya@ikuya.net>
2024-01-16 17:29:43 +0900

    remove unused code

--- train.py
+++ train.py
@@ -3,17 +3,11 @@ from dataclasses import dataclass, field
 
 import datasets
 import transformers
-from datasets import (
-    concatenate_datasets,
-    interleave_datasets,
-    load_dataset,
-    load_from_disk,
-)
+from datasets import concatenate_datasets, load_from_disk
 from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
     HfArgumentParser,
-    TrainerCallback,
     TrainingArguments,
     set_seed,
 )
@@ -30,9 +24,6 @@ class LeiaTrainingArguments(TrainingArguments):
     use_flash_attention_2: bool = field(default=False)
 
     wikipedia_dataset_dir: str | None = field(default=None)
-    text_dataset_path: str | None = field(default=None)
-    text_dataset_name: str | None = field(default=None)
-    text_dataset_sampling_prob: float = field(default=0.0)
 
     trans_insertion_strategy: str = field(default="none")
     trans_insertion_prob: float = field(default=1.0)
@@ -45,13 +36,6 @@ class LeiaTrainingArguments(TrainingArguments):
     max_eval_samples_for_tasks: int | None = field(default=None)
     num_fewshot_samples_for_tasks: str | None = field(default=None)
     use_dynamic_generation_length: bool = field(default=True)
-    eval_at_first_step: bool = field(default=False)
-
-
-class EvaluateFirstStepCallback(TrainerCallback):
-    def on_step_begin(self, args, state, control, **kwargs):
-        if state.global_step == 0:
-            control.should_evaluate = True
 
 
 def main():
@@ -101,39 +85,12 @@ def main():
     wikipedia_dataset_size = len(wikipedia_dataset)
     wikipedia_dataset = wikipedia_dataset.to_iterable_dataset(num_shards=1024)
 
-    if args.text_dataset_path:
-        text_dataset = load_dataset(args.text_dataset_path, name=args.text_dataset_name, split="train", streaming=True)
-
-        text_dataset = text_dataset.map(
-            lambda texts: {"input_ids": tokenizer(texts)["input_ids"]},
-            input_columns=["text"],
-            remove_columns=list(next(iter(text_dataset)).keys()),
-            batched=True,
-            features=datasets.Features({"input_ids": datasets.Sequence(datasets.Value(dtype="int16"))}),
-        )
-
-        train_dataset = interleave_datasets(
-            [wikipedia_dataset, text_dataset],
-            probabilities=[
-                1.0 - args.text_dataset_sampling_prob,
-                args.text_dataset_sampling_prob,
-            ],
-            stopping_strategy="first_exhausted",
-            seed=args.seed,
-        )
-        # We assume that the text dataset is much larger than the Wikipedia dataset
-        train_dataset_size = int(wikipedia_dataset_size / (1.0 - args.text_dataset_sampling_prob))
-
-    else:
-        train_dataset = wikipedia_dataset
-        train_dataset_size = wikipedia_dataset_size
-
     max_num_examples = (
         args.max_steps * args.gradient_accumulation_steps * args.per_device_train_batch_size * args.world_size
     )
     train_cl_dataset = LeiaConstantLengthDataset(
-        train_dataset,
-        dataset_size=train_dataset_size,
+        wikipedia_dataset,
+        dataset_size=wikipedia_dataset_size,
         max_length=args.max_length,
         max_num_examples=max_num_examples,
         trans_start_token_id=tokenizer.vocab["<trans>"],
@@ -151,7 +108,6 @@ def main():
     eval_tasks = []
     if args.eval_tasks is not None:
         eval_tasks = args.eval_tasks.split(",")
-
     num_fewshot_samples_for_tasks = None
     if args.num_fewshot_samples_for_tasks is not None:
         num_fewshot_samples_for_tasks = [int(n) for n in args.num_fewshot_samples_for_tasks.split(",")]
@@ -172,13 +128,7 @@ def main():
             "use_dynamic_generation_length": args.use_dynamic_generation_length,
         },
     )
-    if args.eval_at_first_step:
-        trainer.add_callback(EvaluateFirstStepCallback())
-
-    if args.resume_from_checkpoint is not None:
-        trainer.train(resume_from_checkpoint=args.resume_from_checkpoint)
-    else:
-        trainer.train()
+    trainer.train()
 
     trainer.save_state()
     trainer.save_model()
