19e15bc11f1bd0ca24bc5e7afbd315428c126772
Ikuya Yamada <ikuya@ikuya.net>
2024-02-01 00:27:42 +0900

    fix jcsqa

--- leia/tasks/jcommonsenseqa.py
+++ leia/tasks/jcommonsenseqa.py
@@ -5,9 +5,6 @@ from .base import LogLikelihoodRequest, LoglikelihoodTask
 
 
 class JCommonsenseQA(LoglikelihoodTask):
-    def _get_description(self) -> str:
-        return "以下は、タスクを説明する指示と、文脈のある入力の組み合わせです。要求を適切に満たす応答を書きなさい。\n\n"
-
     def _get_train_dataset(self) -> Dataset:
         return load_dataset("shunk031/JGLUE", "JCommonsenseQA", split="train")
 
@@ -15,11 +12,7 @@ class JCommonsenseQA(LoglikelihoodTask):
         return load_dataset("shunk031/JGLUE", "JCommonsenseQA", split="validation")
 
     def _example_to_text(self, example: dict) -> str:
-        choices = "\n".join([f"- {example[f'choice{index}']}" for index in range(5)])
-        return (
-            f"### 指示:\n与えられた選択肢の中から、最適な答えを選んでください。出力は以下から選択してください：\n{choices}\n\n"
-            f"### 入力:\n{example['question']}\n\n### 応答:\n"
-        )
+        return f"Question: {example['question']}\nAnswer:"
 
     def _example_to_target(self, example: dict) -> str:
         return example[f'choice{example["label"]}']
